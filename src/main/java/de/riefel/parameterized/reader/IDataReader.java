c5d93dd33ae64dc6a79bed344dce0d312990be11
Felix Riess <felix-riess@t-online.de>
2021-03-26 22:15:50 +0100

    interface to read data

---
+++ src/main/java/de/riefel/parameterized/reader/IDataReader.java
@@ -0,0 +1,24 @@
+package de.riefel.parameterized.reader;
+
+import de.riefel.parameterized.common.types.AbstractAbsoluteSpreadComparableTO;
+
+import java.util.List;
+
+/**
+ * Interface for all types of data readers.
+ *
+ * @author Felix Riess <felix@felix-riess.de>
+ * @since 26.03.21
+ */
+public interface IDataReader {
+
+    /**
+     * Read the data from the specified path and return the data as {@link List} of {@link T}.
+     *
+     * @param pathToFile the path to the file which should be read (not {@code null}).
+     * @param lineMapper the lineMapper which should be used to map a line to a object of type {@link T} (not {@code null}).
+     * @param <T> the type to be returned (must extend {@link AbstractAbsoluteSpreadComparableTO}.
+     * @return a {@link List} of the valid data.
+     */
+    <T extends AbstractAbsoluteSpreadComparableTO> List<T> readData(final String pathToFile, final ILineMapper<T> lineMapper);
+}
