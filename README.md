d5fdaf6ff08d1d3897167d80c1890acadf49099f
Felix Riess <felix-riess@t-online.de>
2021-03-26 21:41:43 +0100

    Add csv files and a main app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Parameterized Properties
+This is a little example application which parses input from CSV files (or other formats) and makes some calculations based on the data.
+The goal of this project is to implement parameterized properties which validate the provided data.
+## Run the app
+#### Build and unit-test the app
+```mvn verify```
+#### To run the app
+```mvn exec:java```
---
+++ src/main/java/de/riefel/parameterized/App.java
@@ -0,0 +1,18 @@
+package de.riefel.parameterized;
+
+/**
+ * The entry class for this test application.
+ *
+ * @author Felix Riess <felix@felix-riess.de>
+ * @since 26.03.21
+ */
+public class App {
+
+    /**
+     * The main entry method of the app.
+     * @param args optional command line arguments
+     */
+    public static void main(String...args) {
+        // TODO [FR] (26.03.2021): do something useful
+    }
+}
