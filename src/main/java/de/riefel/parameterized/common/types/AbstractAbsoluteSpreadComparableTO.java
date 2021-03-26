9e0f7048e9976c1d53c4c1631536661e4623264d
Felix Riess <felix-riess@t-online.de>
2021-03-26 22:04:39 +0100

    implement abstract class to compare two absolute spreads

---
+++ src/main/java/de/riefel/parameterized/common/types/AbstractAbsoluteSpreadComparableTO.java
@@ -0,0 +1,48 @@
+package de.riefel.parameterized.common.types;
+
+import java.io.Serializable;
+
+/**
+ * Abstract base class which implements {@link Comparable} to compare two absolute spreads.
+ *
+ * @author Felix Riess <felix@felix-riess.de>
+ * @since 26.03.21
+ */
+public abstract class AbstractAbsoluteSpreadComparableTO implements Comparable<AbstractAbsoluteSpreadComparableTO>, Serializable {
+    /**
+     * generated serialVersionUID.
+     */
+    private static final long serialVersionUID = 7234615714945883566L;
+
+    private final int min;
+    private final int max;
+
+    /**
+     * Constructor to initialize min and max for spread comparison.
+     *
+     * @param min the min value.
+     * @param max the max value.
+     */
+    protected AbstractAbsoluteSpreadComparableTO(final int min, final int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    private int getAbsoluteSpread() {
+        return Math.abs(this.max - this.min);
+    }
+
+    @Override
+    public int compareTo(AbstractAbsoluteSpreadComparableTO other) {
+        return Integer.compare(getAbsoluteSpread(), other.getAbsoluteSpread());
+    }
+
+    @Override
+    public abstract int hashCode();
+
+    @Override
+    public abstract boolean equals(Object o);
+
+    @Override
+    public abstract String toString();
+}
