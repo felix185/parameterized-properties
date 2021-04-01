9f4ff003d40490a5ac3731fcd947c68f94eb532c
Felix Riess <felix-riess@t-online.de>
2021-04-01 19:27:48 +0200

    add DoubleProperty

---
+++ src/main/java/de/riefel/parameterized/common/property/DoubleProperty.java
@@ -0,0 +1,61 @@
+package de.riefel.parameterized.common.property;
+
+import de.riefel.parameterized.common.validation.ArgumentChecker;
+
+/**
+ * Parameterized property for {@link Double} types.
+ *
+ * @author Felix Riess <felix@felix-riess.de>
+ * @since 01.04.21
+ */
+public class DoubleProperty extends AbstractProperty {
+
+    private final double minValue;
+    private final double maxValue;
+
+    /**
+     * Constructor.
+     *
+     * @param clazz the associated class of this property (not {@code null}).
+     * @param name the name of the property (not {@code null} or empty).
+     * @param minValue the min value of the attribute ({@code minValue <= maxValue}).
+     * @param maxValue the max value of the attribute ({@code minValue <= minValue}).
+     * @param attributes additional {@link PropertyAttribute}s.
+     */
+    public DoubleProperty(final Class<?> clazz, final String name, final double minValue, final double maxValue, final PropertyAttribute...attributes) {
+        super(clazz, name, attributes);
+
+        ArgumentChecker.checkIsTrue(minValue<=maxValue, "Min value {} is greater than max value {}", minValue, maxValue);
+        this.maxValue = maxValue;
+        this.minValue = minValue;
+    }
+
+    /**
+     * Constructor. Sets minValue to {@link Double#MIN_VALUE} and maxValue to {@link Double#MAX_VALUE}.
+     *
+     * @param clazz the associated class of this property (not {@code null}).
+     * @param name the name of the property (not {@code null} or empty).
+     * @param attributes additional {@link PropertyAttribute}s.
+     */
+    public DoubleProperty(final Class<?> clazz, final String name, final PropertyAttribute...attributes) {
+        this(clazz, name, Double.MIN_VALUE, Double.MAX_VALUE, attributes);
+    }
+
+    /**
+     * Get minValue
+     *
+     * @return value of minValue
+     */
+    public double getMinValue() {
+        return this.minValue;
+    }
+
+    /**
+     * Get maxValue
+     *
+     * @return value of maxValue
+     */
+    public double getMaxValue() {
+        return this.maxValue;
+    }
+}
